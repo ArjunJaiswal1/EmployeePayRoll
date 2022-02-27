5d40617637c34d3b3652e72237549f68b9437bf9
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-27 15:53:07 +0530

    [Arjun2] Add; Added the method to create table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,12 @@
 Create database Payroll_Service;
 use payroll_Service;
+
+---UC2 Create Table---
+create table employee_payroll
+(
+ id int identity(1,1) primary key,
+ name varchar(100),
+ salary float,
+ startdate date,
+);
+
