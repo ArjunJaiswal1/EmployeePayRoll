7a784e2142372eb390b534a5ddad49278c0c5466
ArjunJaiswal1 <97870068+ArjunJaiswal1@users.noreply.github.com>
2022-03-02 10:34:13 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -11,17 +11,17 @@ create table employee_payroll
 );
 
 ---UC3 Create Employee payroll database service as a part of CURD operation---
-INSERT INTO employee_payroll (Name, Salary, Startdate) VALUES ('Mohammed', 45986.58, getdate());
-INSERT INTO employee_payroll  VALUES ('Sajid', 95986.58, getdate());
-INSERT INTO employee_payroll  VALUES ('Sakil', 685986.58, getdate());
-INSERT INTO employee_payroll (Name, startdate) VALUES ('Roy', getdate());
+INSERT INTO employee_payroll (Name, Salary, Startdate) VALUES ('Arjun', 45986.58, getdate());
+INSERT INTO employee_payroll  VALUES ('Ramesh', 95986.58, getdate());
+INSERT INTO employee_payroll  VALUES ('Rakesh', 685986.58, getdate());
+INSERT INTO employee_payroll (Name, startdate) VALUES ('Sonu', getdate());
 
 --- UC4 Ability to retrieve all the data that is added in payroll service database----
-select * from employee_payroll where Name= 'Sajid';
-select id,Name from employee_payroll where Name= 'Sajid';
+select * from employee_payroll where Name= 'Ramesh';
+select id,Name from employee_payroll where Name= 'Ramesh';
 
 ---UC5 Ability to retrieve salary data for a particular employee-----
-select salary from employee_payroll where Name= 'Sajid';
+select salary from employee_payroll where Name= 'Ramesh';
 select * from employee_payroll;
 select salary,Name from employee_payroll where startdate between CAST('2021-02-02' as Date) and getdate();
 
